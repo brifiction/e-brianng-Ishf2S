89b348f08bb6c93753f004bb0da70c6170bb819d
Brian Ng <brianngchongeu@gmail.com>
2025-08-10 16:13:48 +1000

    chore: removed hello world examples & added ts path alias

---
+++ src/components/RaceStart.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Races starting soon</h1>
+
+  <div class="card">
+    <ul>
+      <li>Race 1</li>
+      <li>Race 2</li>
+      <li>Race 3</li>
+      <li>Race 4</li>
+      <li>Race 5</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 0.5rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 0.5rem;
+}
+</style>
